b2b1eb1cef112e003e21bbafa5793743ae95ff2f
abdullah <abdullahmafuz1@gmail.com>
2019-07-24 00:21:03 +0400

    Updates

---
+++ precache-manifest.10f956320eb011b9023a21b1dfa83441.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc299586bf820c32f69687dbf32a0872",
+    "url": "/chaseandhunt/index.html"
+  },
+  {
+    "revision": "a83097d8037404adaa9c",
+    "url": "/chaseandhunt/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ad34ce2b58d3a342ce36",
+    "url": "/chaseandhunt/static/css/main.40990922.chunk.css"
+  },
+  {
+    "revision": "a83097d8037404adaa9c",
+    "url": "/chaseandhunt/static/js/2.bc59552e.chunk.js"
+  },
+  {
+    "revision": "ad34ce2b58d3a342ce36",
+    "url": "/chaseandhunt/static/js/main.302b733b.chunk.js"
+  },
+  {
+    "revision": "2936c58fa7b8f4d172db",
+    "url": "/chaseandhunt/static/js/runtime~main.352ff068.js"
+  },
+  {
+    "revision": "9222555a2d92f76eb0a5d1629fc4e7c4",
+    "url": "/chaseandhunt/static/media/1.9222555a.png"
+  },
+  {
+    "revision": "114996e78691ab01e87bd65152abd093",
+    "url": "/chaseandhunt/static/media/2.114996e7.jpg"
+  },
+  {
+    "revision": "91f260485e88d91a285ccf49e1a04fd7",
+    "url": "/chaseandhunt/static/media/3.91f26048.jpg"
+  },
+  {
+    "revision": "08987220cb34a3c275adcc5662600718",
+    "url": "/chaseandhunt/static/media/4.08987220.jpg"
+  },
+  {
+    "revision": "b232dbd7bf71795f46d7d7cdb3ce4103",
+    "url": "/chaseandhunt/static/media/Damac-Logo.b232dbd7.png"
+  },
+  {
+    "revision": "18499f52ee3d4626427dd07f1f231bf6",
+    "url": "/chaseandhunt/static/media/Emami.18499f52.png"
+  },
+  {
+    "revision": "5b865daf59f23bf61b148c1fc7e1f761",
+    "url": "/chaseandhunt/static/media/Emirites-NBD.5b865daf.png"
+  },
+  {
+    "revision": "2ebd2ebba26c502ae0780c6152f2f08c",
+    "url": "/chaseandhunt/static/media/bg-log.2ebd2ebb.png"
+  },
+  {
+    "revision": "83360f82e2f913925d01bf79d7673a60",
+    "url": "/chaseandhunt/static/media/chase&hunt_logo.83360f82.png"
+  },
+  {
+    "revision": "52eac568958bed4ce3cb00c5f251719c",
+    "url": "/chaseandhunt/static/media/chaseandhuntmap.52eac568.jpg"
+  },
+  {
+    "revision": "1b925d5793c06e689d537dee601d6b3c",
+    "url": "/chaseandhunt/static/media/emirate.1b925d57.jpg"
+  },
+  {
+    "revision": "601dae16b5d14adce933f9f7019e3ee9",
+    "url": "/chaseandhunt/static/media/icare.601dae16.png"
+  },
+  {
+    "revision": "620c8c1ed1488627729f1e6323384ff1",
+    "url": "/chaseandhunt/static/media/img1.620c8c1e.jpg"
+  },
+  {
+    "revision": "6b12742b36db7fe367daeaa1f897eccc",
+    "url": "/chaseandhunt/static/media/img2.6b12742b.jpg"
+  },
+  {
+    "revision": "3fa9877300d9a1509b173b2b28c83681",
+    "url": "/chaseandhunt/static/media/img3.3fa98773.jpg"
+  },
+  {
+    "revision": "ceb146c5cc7cca1171dfbb56266dc4c7",
+    "url": "/chaseandhunt/static/media/job.ceb146c5.jpg"
+  },
+  {
+    "revision": "bd7a5b2372f35c303072f4d8fc18e326",
+    "url": "/chaseandhunt/static/media/masafi.bd7a5b23.png"
+  },
+  {
+    "revision": "bc0da9282f41bddaf1fa42d649119d75",
+    "url": "/chaseandhunt/static/media/p1.bc0da928.jpg"
+  },
+  {
+    "revision": "fb375663b18d137bacfcc5864b754e1f",
+    "url": "/chaseandhunt/static/media/p2.fb375663.jpg"
+  },
+  {
+    "revision": "aebed59faff5bc55650a7b50c50e5d77",
+    "url": "/chaseandhunt/static/media/p3.aebed59f.jpg"
+  },
+  {
+    "revision": "3eacf0a440c1170822d343eaf5403add",
+    "url": "/chaseandhunt/static/media/p4.3eacf0a4.jpg"
+  },
+  {
+    "revision": "da18ecb5f1516db9e019c317b1eb3766",
+    "url": "/chaseandhunt/static/media/post1.da18ecb5.jpg"
+  },
+  {
+    "revision": "53766d3d8d867f4df33ba0ca69fcb7d9",
+    "url": "/chaseandhunt/static/media/post2.53766d3d.jpg"
+  },
+  {
+    "revision": "082b7f50555355240e201266ab044dca",
+    "url": "/chaseandhunt/static/media/post3.082b7f50.jpg"
+  },
+  {
+    "revision": "d8a2a50f53985ed92a01c175f63bd665",
+    "url": "/chaseandhunt/static/media/retail.d8a2a50f.jpg"
+  },
+  {
+    "revision": "f6260a45b9f88bcaff36ed92337fd9d5",
+    "url": "/chaseandhunt/static/media/s2.f6260a45.png"
+  },
+  {
+    "revision": "f04a3f5a61e8da0f470f03605b0d535d",
+    "url": "/chaseandhunt/static/media/s3.f04a3f5a.png"
+  },
+  {
+    "revision": "f2c7ab6157030631446f04d146f9fe4f",
+    "url": "/chaseandhunt/static/media/vacancy.f2c7ab61.jpg"
+  },
+  {
+    "revision": "a58936eb51816820cb512c437a0bcd15",
+    "url": "/chaseandhunt/static/media/wasl.a58936eb.jpg"
+  },
+  {
+    "revision": "6cc56b4d3df6ee5c02caef8846ec34d1",
+    "url": "/chaseandhunt/static/media/zulekha.6cc56b4d.png"
+  }
+]);
